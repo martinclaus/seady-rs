ba0b1e9226831ec6c587985f21a41ca4be5441e1
Martin Claus <mclaus@geomar.de>
2022-11-24 13:19:30 +0100

    Switch State to wrap fixed-map

--- src/state.rs
+++ src/state.rs
@@ -1,101 +1,75 @@
-//! Data structures and traits to describe the state of an dynamical system
+//! Data struc0s and traits to describe the state of an dynamical system
 
+use fixed_map::{Key, Map};
 use std::{
     collections::VecDeque,
-    hash::Hash,
-    marker::PhantomData,
     ops::{Index, IndexMut},
     rc::Rc,
 };
 
 use crate::{mask::Mask, variable::Variable};
 
+/// Marker trait for Variable keys of [State] collections
+pub trait VarKey: Copy + fixed_map::key::Key + std::fmt::Debug {}
+
 /// Prognostic variables of the shallow water equations
-#[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
+#[derive(Copy, Clone, Key, Debug)]
 pub enum SWMVars {
     U,
     V,
     ETA,
 }
 
-/// Trait for sets of [`State`] variable identifier
-///
-/// A type that implements VarSet must provided a unique index for each identifier starting at 0.
-/// Typically this can be achieved by implementing the trait on an enum which must not use
-/// [custom discriminant values](https://doc.rust-lang.org/reference/items/enumerations.html#custom-discriminant-values-for-fieldless-enumerations).
-pub trait VarSet: Sized + Copy {
-    fn values() -> &'static [Self];
-    fn as_usize(&self) -> usize;
-}
-
-impl VarSet for SWMVars {
-    fn values() -> &'static [Self] {
-        &[Self::U, Self::V, Self::ETA][..]
-    }
-
-    fn as_usize(&self) -> usize {
-        *self as usize
-    }
-}
+impl VarKey for SWMVars {}
 
 /// State of a system.
 ///
 /// The state is a collection of all prognostic variables of an set of differential equations, that are
 /// the variables which for the solution of the system.
 #[derive(Debug)]
-pub struct State<K, V> {
-    vars: Box<[V]>,
-    var_set: PhantomData<K>,
-}
+pub struct State<K, V>(Map<K, V>)
+where
+    K: VarKey;
 
 impl<K, V> Index<K> for State<K, V>
 where
-    K: VarSet,
+    K: VarKey,
 {
     type Output = V;
 
     fn index(&self, index: K) -> &Self::Output {
-        assert!(self.vars.len() > index.as_usize());
-        &self.vars[index.as_usize()]
+        &self
+            .0
+            .get(index)
+            .expect(&format!("Variable {index:?} should be in state"))
     }
 }
 
 impl<K, V> IndexMut<K> for State<K, V>
 where
-    K: VarSet,
+    K: VarKey,
 {
     fn index_mut(&mut self, index: K) -> &mut Self::Output {
-        assert!(self.vars.len() > index.as_usize());
-        &mut self.vars[index.as_usize()]
+        self.0
+            .get_mut(index)
+            .expect(&format!("Variable {index:?} should be in state"))
     }
 }
 
-/// Type alias for tuples of variable id and grid to define the variable on.
-pub type GridMap<K, G> = (K, Rc<G>);
+/// Type alias for mapping variable id onto the grid on which the variable is defined.
+pub type GridMap<K, G> = Map<K, Rc<G>>;
 
 impl<K, V> State<K, V>
 where
-    K: VarSet + Clone,
+    K: VarKey,
 {
-    /// Create a new state from an slice of grid mappings.
-    ///
-    /// Note that `grid_map` will be sorted such that the variables created in `self.vars` will be inserted
-    /// the ordering implied by [VarSet::as_usize].
-    pub fn new<I, M>(grid_map: &[GridMap<K, <V as Variable<I, M>>::Grid>]) -> Self
+    /// Create a new state from a grid mapping.
+    pub fn new<I, M>(grid_map: &GridMap<K, <V as Variable<I, M>>::Grid>) -> Self
     where
         V: Variable<I, M>,
         M: Mask,
     {
-        let mut grid_map = grid_map.to_vec();
-        grid_map.sort_by_key(|a| a.0.as_usize());
-        State {
-            vars: grid_map
-                .iter()
-                .map(|(_, g)| V::zeros(g))
-                .collect::<Vec<_>>()
-                .into_boxed_slice(),
-            var_set: PhantomData,
-        }
+        State(grid_map.iter().map(|(k, g)| (k, V::zeros(g))).collect())
     }
 }
 
@@ -109,8 +83,9 @@ pub struct StateFactory<K, V, I, M>
 where
     V: Variable<I, M>,
     M: Mask,
+    K: VarKey,
 {
-    grid_map: Box<[GridMap<K, V::Grid>]>,
+    grid_map: GridMap<K, V::Grid>,
     buffer: VecDeque<State<K, V>>,
 }
 
@@ -118,11 +93,12 @@ impl<K, V, I, M> StateFactory<K, V, I, M>
 where
     V: Variable<I, M>,
     M: Mask,
+    K: VarKey,
 {
     /// Create a new StateFactory object
-    pub fn new(grid_map: impl Iterator<Item = GridMap<K, V::Grid>>) -> Self {
+    pub fn new(grid_map: impl IntoIterator<Item = (K, Rc<V::Grid>)>) -> Self {
         Self {
-            grid_map: grid_map.collect::<Vec<_>>().into_boxed_slice(),
+            grid_map: grid_map.into_iter().collect(),
             buffer: VecDeque::new(),
         }
     }
@@ -136,23 +112,17 @@ where
     }
 
     /// Create a new state object. This call involves memory allocation.
-    fn make_state(&self) -> State<K, V>
-    where
-        K: VarSet,
-    {
+    fn make_state(&self) -> State<K, V> {
         State::new(&self.grid_map)
     }
 
     /// Return a state object. The object is either popped form the internal buffer of allocated.
-    /// **No assumptions can be made about the content of the data containeed in the state's variables**.
-    pub fn get(&mut self) -> State<K, V>
-    where
-        K: VarSet,
-    {
+    /// **No assumptions can be made about the content of the data contained in the state's variables**.
+    pub fn get(&mut self) -> State<K, V> {
         self.buffer.pop_front().unwrap_or_else(|| self.make_state())
     }
 
-    /// Push a state objec to the internal buffer for later reuse.
+    /// Push a state object to the internal buffer for later reuse.
     pub fn take(&mut self, state: State<K, V>) {
         self.buffer.push_back(state);
     }
@@ -165,16 +135,17 @@ where
 /// dropped or consumed by a [StateFactory]'s internal buffer.
 /// New elements will be added to the end of the deque.
 #[derive(Debug)]
-pub struct StateDeque<K, V> {
-    inner: VecDeque<State<K, V>>,
-}
+pub struct StateDeque<K, V>(VecDeque<State<K, V>>)
+where
+    K: VarKey;
 
-impl<K, V> StateDeque<K, V> {
+impl<K, V> StateDeque<K, V>
+where
+    K: VarKey,
+{
     /// Create a new StateDeque with a given fixed capacity.
     pub fn new(capacity: usize) -> StateDeque<K, V> {
-        StateDeque {
-            inner: VecDeque::<State<K, V>>::with_capacity(capacity),
-        }
+        StateDeque(VecDeque::<State<K, V>>::with_capacity(capacity))
     }
 
     /// Add an element to the end of the deque. If the capacity is reached, the oldest element will be droped or
@@ -186,59 +157,67 @@ impl<K, V> StateDeque<K, V> {
         K: Copy,
     {
         // buffer full
-        if self.inner.len() == self.inner.capacity() {
-            self.inner.pop_front().and_then(|state| {
+        if self.0.len() == self.0.capacity() {
+            self.0.pop_front().and_then(|state| {
                 if let Some(state_factory) = consumer {
                     state_factory.take(state);
                 }
                 Some(())
             });
         }
-        self.inner.push_back(elem);
+        self.0.push_back(elem);
     }
 
     /// Number of elements.
     pub fn len(&self) -> usize {
-        self.inner.len()
+        self.0.len()
     }
 
     /// Returns true if there are no elements in the deque.
     pub fn is_empty(&self) -> bool {
-        self.inner.is_empty()
+        self.0.is_empty()
     }
 
     /// Returns a [VecDeque<&V>] with references to a particular variable of all contained states.
-    pub fn get_var(&self, var: K) -> VecDeque<&V>
-    where
-        K: VarSet,
-    {
-        VecDeque::from_iter(self.inner.iter().map(|s| &s[var]))
+    pub fn get_var(&self, var: K) -> VecDeque<&V> {
+        VecDeque::from_iter(self.0.iter().map(|s| &s[var]))
     }
 }
 
-impl<K, V> AsRef<VecDeque<State<K, V>>> for StateDeque<K, V> {
+impl<K, V> AsRef<VecDeque<State<K, V>>> for StateDeque<K, V>
+where
+    K: VarKey,
+{
     fn as_ref(&self) -> &VecDeque<State<K, V>> {
-        &self.inner
+        &self.0
     }
 }
 
-impl<K, V> Index<usize> for StateDeque<K, V> {
+impl<K, V> Index<usize> for StateDeque<K, V>
+where
+    K: VarKey,
+{
     type Output = State<K, V>;
 
     fn index(&self, index: usize) -> &Self::Output {
-        self.inner.index(index)
+        self.0.index(index)
     }
 }
 
-impl<K, V> IndexMut<usize> for StateDeque<K, V> {
+impl<K, V> IndexMut<usize> for StateDeque<K, V>
+where
+    K: VarKey,
+{
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
-        self.inner.index_mut(index)
+        self.0.index_mut(index)
     }
 }
 
 #[cfg(test)]
 mod test {
     use std::rc::Rc;
+
+    use fixed_map::Key;
 
     use crate::{
         field::{Arr2D, Field},
@@ -247,22 +226,14 @@ mod test {
         variable::Var,
     };
 
-    use super::{StateDeque, StateFactory, VarSet};
+    use super::{StateDeque, StateFactory, VarKey};
 
-    #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+    #[derive(Clone, Copy, Key, Debug)]
     enum SingleVar {
         Var,
     }
 
-    impl VarSet for SingleVar {
-        fn values() -> &'static [Self] {
-            &[SingleVar::Var]
-        }
-
-        fn as_usize(&self) -> usize {
-            *self as usize
-        }
-    }
+    impl VarKey for SingleVar {}
 
     fn make_simple_state_factory(
     ) -> StateFactory<SingleVar, Var<Arr2D<f64>, Grid2D<f64, DomainMask>>, f64, DomainMask> {
@@ -271,14 +242,14 @@ mod test {
 
         let grid_map = (SingleVar::Var, Rc::new(grid));
 
-        StateFactory::new([grid_map].into_iter())
+        StateFactory::new([grid_map])
     }
 
     #[test]
     fn state_index_returns_variable() {
         let state = make_simple_state_factory().make_state();
 
-        assert_eq!(state.vars.len(), 1);
+        assert_eq!(state.0.len(), 1);
 
         let _ = &state[SingleVar::Var];
     }
